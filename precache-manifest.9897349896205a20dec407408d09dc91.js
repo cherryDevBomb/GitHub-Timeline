d76936a77255b4ccae3c47f2ea413ff6b682cf62
cherryDevBomb <turcan.olguta@gmail.com>
2020-08-16 01:12:02 +0300

    Updates

---
+++ precache-manifest.9897349896205a20dec407408d09dc91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "691851fbfb434503ef57a7af82ede746",
+    "url": "/GitHub-Timeline/index.html"
+  },
+  {
+    "revision": "b80c857c95aa2cf843fc",
+    "url": "/GitHub-Timeline/static/css/2.5081701e.chunk.css"
+  },
+  {
+    "revision": "fee6b05b604e3b82d41f",
+    "url": "/GitHub-Timeline/static/css/main.9478c168.chunk.css"
+  },
+  {
+    "revision": "b80c857c95aa2cf843fc",
+    "url": "/GitHub-Timeline/static/js/2.77b49eaa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/GitHub-Timeline/static/js/2.77b49eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee6b05b604e3b82d41f",
+    "url": "/GitHub-Timeline/static/js/main.4c41ccd7.chunk.js"
+  },
+  {
+    "revision": "b49a8f2b06e0a5aa87f2",
+    "url": "/GitHub-Timeline/static/js/runtime-main.495c8a94.js"
+  }
+]);
