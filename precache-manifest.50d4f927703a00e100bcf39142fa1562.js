0052357f3e5a036561bfc76be2bccfaa437d4e56
cherryDevBomb <turcan.olguta@gmail.com>
2020-08-16 17:44:00 +0300

    Updates

---
+++ precache-manifest.50d4f927703a00e100bcf39142fa1562.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579bdc159a16809cfa9e829c24483ac1",
+    "url": "/GitHub-Timeline/index.html"
+  },
+  {
+    "revision": "b80c857c95aa2cf843fc",
+    "url": "/GitHub-Timeline/static/css/2.5081701e.chunk.css"
+  },
+  {
+    "revision": "5a364fdbaf11403a2791",
+    "url": "/GitHub-Timeline/static/css/main.f3c545e5.chunk.css"
+  },
+  {
+    "revision": "b80c857c95aa2cf843fc",
+    "url": "/GitHub-Timeline/static/js/2.77b49eaa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/GitHub-Timeline/static/js/2.77b49eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a364fdbaf11403a2791",
+    "url": "/GitHub-Timeline/static/js/main.8fff658e.chunk.js"
+  },
+  {
+    "revision": "b49a8f2b06e0a5aa87f2",
+    "url": "/GitHub-Timeline/static/js/runtime-main.495c8a94.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-timeline/precache-manifest.1f1a1a43e8d5028d54f196bc9ac753c7.js"
+  "/GitHub-Timeline/precache-manifest.50d4f927703a00e100bcf39142fa1562.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-timeline/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/GitHub-Timeline/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
